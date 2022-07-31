088a10e2aa9b2bf148e5e60f0f516d9088d53698
Muhammad Saad Hassan <88556409+saadhassan2002@users.noreply.github.com>
2022-07-31 11:14:02 +0500

    Books Upload

---
+++ Assignment_03.py
@@ -0,0 +1,9 @@
+#Q1
+message = "Hello, world!"
+print(message)
+# ---------------------------
+
+#Q2
+author = "Albert Einstein"
+quote = "A person who never made a mistake never tried anything new."
+print(author,"Once said:", quote)
