59768c31fed0655fc5f57cd174eac8e577e5f0d7
Muhammad Saad Hassan <88556409+saadhassan2002@users.noreply.github.com>
2022-08-13 00:03:26 +0500

    Books

--- third.py
+++ third.py
@@ -1,27 +1,59 @@
-#regression
-sizes = [523,645,708,1034,2290,2545]
-prices = [115,150,210,280,255,440]
+# #regression
+# sizes = [523,645,708,1034,2290,2545]
+# prices = [115,150,210,280,255,440]
 
-c= 0;
-# proce = size * m + c;
+# c= 0;
+# # proce = size * m + c;
 
-# m = price/size * class
-multipliers = []
+# # m = price/size * class
+# multipliers = []
 
-m = 0
-loc = 0
-for price in prices:
-    m = price/sizes[loc]
-    multipliers.append(m)
-    loc=loc+1
+# m = 0
+# loc = 0
+# for price in prices:
+#     m = price/sizes[loc]
+#     multipliers.append(m)
+#     loc=loc+1
 
-multipliers = sum(multipliers)/len(multipliers)
-print(multipliers)
+# multipliers = sum(multipliers)/len(multipliers)
+# print(multipliers)
 
-price = 0.21734 * 523 + c
-print(price)
+# price = 0.21734 * 523 + c
+# print(price)
 
-for loc in range(len(prices)):
-    price = m * sizes[loc]
-    print("Predicted:",price," Actual Prices:",prices[loc])
+# for loc in range(len(prices)):
+#     price = m * sizes[loc]
+#     print("Predicted:",price," Actual Prices:",prices[loc])
+
+
+
+# -----------------------------------------------------------
+
+# rankings = {5: "Norway", 2: "South Africa",	6: "North Africa",	7: "South Africa",	1: "North Africa"}
+# print(rankings)
+
+
+#-------------------------------------------------------------
+# duplication not allowed
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+# print(thisdict)
+# print(thisdict.get("brand"))
+# print(thisdict["model"])
+# print(len(thisdict))
+
+# ---------------------------------------------------------------------
+# Python Collections (Arrays)
+# There are four collection data types in the Python programming language:
+
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
+
+#------------------------------------------------------------------------------
+
 
